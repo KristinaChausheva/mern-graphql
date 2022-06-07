0e274882f5063a5801c68ed118538070a7994af8
Kristina Chausheva <k.chausheva@gmail.com>
2022-06-08 00:27:57 +0300

    running server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+require("dotenv").config()
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
